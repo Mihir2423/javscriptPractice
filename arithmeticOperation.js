507d496e02c33a79b71d790f9445429a40193b64
Anubhav6393 <mishraanubhav8126@gmail.com>
2023-07-12 19:17:43 +0530

    updated

--- arithmeticOperation.js
+++ arithmeticOperation.js
@@ -1,8 +1,18 @@
 // Write short notes about aritmetic operators in js(in comments)
-
+/*
+Airthmetic operators is a combination of operands(values,variables,etc.......)
+operators are(+ - * / %)
+*/
 // Declare two variable a, b and assign them integer values respectively
+let a = 7;
+let b = 3;
 // Perform all the Aritmetic operation to them (*, /, +, -, %)
-
+//a = a + b;
+//a = a- b;
+//a = a * b;
+a = a / b;
+//a = a % 2;
 // Print all the results like this example : 
 // if + operation is done -> Output : Addition Result 10 
 // (where 10 is the sum of a + b)
+console.log(a);
--- variable.js
+++ variable.js
@@ -3,20 +3,24 @@ console.log("Hello World");
 
 // Write short notes about variables(comment these answers) : 
 // - what?
+/*A variable is container for storing data.....
+A variables behaves if it was the value it contains....*/
 
 // - eg. of how to declare, and assign value
 let age = 20;//for integer declaration and assingning value
-let FirstName = "Anubhav";// for string
+let FirstName1 = "Anubhav";// for string
 let student = true; // for boolean
 
 // Test
 console.log(age);
-console.log(FirstName);
+console.log(FirstName1);
 console.log(student);
 // Declare a variable of name "FirstName" and another variable of name "LastName" and assign their value respectively
- 
+let FirstName = "Anu";
+let LastName = "Pandit";
 // Print the variable according to this example :  My name is Anu Bhav
-
+console.log("My name is", FirstName ,"Bhav")
 // Extra
 // - Create one <p> element in html and give it a id = "p"
 // - Select that id as shown on the video and make its value "Hello Paragraph"
+document.getElementById("p").innerHTML="Hello Paragraph";
