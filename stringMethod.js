1fa2c1d9ea0a24a3a5dd31f48a8534e83bea5c1f
Anubhav6393 <mishraanubhav8126@gmail.com>
2023-07-13 21:30:57 +0530

    updatedstring

--- stringMethod.js
+++ stringMethod.js
@@ -1,7 +1,28 @@
 // Show all the string slicing methods
 
 // Take a string input from user
+
 // Show its value in screen
 // Show its length in a p tag in screen
 // Show its value after changing it to uppercase
 // Clear btn to clear everything
+document.getElementById("mybutton").onclick = function(){
+   let text = document.getElementById("mytext").value;
+    document.getElementById("text").innerHTML = text;
+}
+document.getElementById("button1").onclick = function(){
+    let text = document.getElementById("mytext").value;
+    document.getElementById("newtext").innerHTML = "String Length:"+ text.length;
+}
+document.getElementById("button2").onclick = function(){
+    let text = document.getElementById("mytext").value;
+    document.getElementById("newtext").innerHTML = "String in uppercase:"+ text.toUpperCase();
+}
+document.getElementById("button3").onclick = function(){
+    let text = document.getElementById("mytext").value;
+    document.getElementById("newtext").innerHTML = "String in lowercase:"+ text.toLowerCase();
+}
+document.getElementById("button4").onclick = function(){
+    document.getElementById("mytext").innerHTML = "";
+    document.getElementById("newtext").innerHTML = "";
+}
--- userInput.js
+++ userInput.js
@@ -1,2 +1,11 @@
 // Repeat the things done in video 
+//we can take input by user in 2 types
+//1st way= Easy way with a window prompt
+let username= window.prompt ("What's your name?");
+console.log(username);
+//Difficult Way Html Textbox
+let username;
+document.getElementById("").onclick = function{
 
+} 
+//let username1= document.getElementById("username1").value;
