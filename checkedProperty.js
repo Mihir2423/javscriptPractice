842a858ce14881f0c55d9d18774687abdc59f6b8
Anubhav6393 <mishraanubhav8126@gmail.com>
2023-07-19 23:26:51 +0530

    updates

--- checkedProperty.js
+++ checkedProperty.js
@@ -1,3 +1,19 @@
 // In html make a form containing, two input and a checkbox and a submit btn
 // Inputs should take store integer
 // If the checkbox is checked perform addition, else perform multiplication
+
+document.getElementById("mybtn").onclick = function(){
+    let num1 = document.getElementById("number1").value;
+    let num2 = document.getElementById("number2").value;
+    let checkbox = document.getElementById("mycheckbox");
+    let result;
+
+    if(document.getElementById("checkbox".checked)){
+         result = num1 + num2;
+         console.log(result);
+    } 
+    else {
+        result = num1 * num2;
+        console.log(result);
+    }
+}
--- methodChaining.js
+++ methodChaining.js
@@ -1,3 +1,10 @@
 // Implement method chaining to make the second character of a string capital and print it
 // eg : "string"
 // Output : T
+
+let username =  "string";
+
+let letter = username.charAt(1);
+letter = letter.toUpperCase();
+
+console.log(letter);
