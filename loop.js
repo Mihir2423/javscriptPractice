410015552abff210b38065d89e030682a45a7993
Anubhav6393 <mishraanubhav8126@gmail.com>
2023-07-20 20:05:52 +0530

    update loop

--- loop.js
+++ loop.js
@@ -3,3 +3,18 @@
 // if number is a multiple of 13 dont print it (apply continue statement)
 // if the number is equal to you variable, break out from the loop.
 
+
+const randomnumber = Math.floor(Math.random() * (99 - 30 + 1)) + 30;
+
+console.log("Random Integer:", randomnumber);
+
+for (let number = 1; number <= 100; number++) {
+    if (number % 13 === 0) {
+      continue;
+    }
+    console.log(number);
+    if (number === randomnumber) {
+      console.log("Break the Loop:");
+      break;
+    }
+  }
