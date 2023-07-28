54f4510d175e4da7efc044e71adc62850184f7a5
Anubhav6393 <mishraanubhav8126@gmail.com>
2023-07-28 19:09:30 +0530

    updates

--- function.js
+++ function.js
@@ -1,3 +1,18 @@
 // Create a function that store two variables name and age
 // Create another function that returns Old if age is greater than 18 else returns Young
 // At last print : Name is Old/Young based on the variables value 
+let username;
+let userage;
+
+function store(username , userage){
+    username = name;
+    userage = age; 
+}
+function checkuserageAndprint() {
+    if (userage > 18) {
+      console.log(username + " is Old");
+    }
+    else {
+      console.log(username + " is Young");
+    }
+  }
