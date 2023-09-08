22ecc1c6e89e0a0eded49d46d000886f19cebbbc
Anubhav6393 <mishraanubhav8126@gmail.com>
2023-09-09 01:50:43 +0530

    check

--- callBack.js
+++ callBack.js
@@ -1,5 +1,22 @@
 // Define Callback Function 
 // Create a function that accept three paramter, two integers(a,b) and one callback function (display)
 // Inside this function return the function with display
+//Call back function== A function passed as an Argument to another function.......
+sum (2 , 3 , displayconsole);
+
+function sum(x ,y ,callBack){
+    let result = x + y;
+    callBack(result);
+}
+function displayconsole (output){
+    console.log(output);
+}
+function displayDOM(output){
+    document.getElementById("mylabel").innerHTML = output;
+}
+
+
+
+
 
 // this display function takes a single parameter and prints it
